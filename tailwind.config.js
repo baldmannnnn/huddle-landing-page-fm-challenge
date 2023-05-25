c4a6e393ef0315f13b0f5b3c821c8c5b0d5293cc
Baldo <baldman1130@gmail.com>
2023-05-25 23:58:47 +0800

    fix for medium screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '640px',
+      lg: '768px',
+      xl: '976px',
+      '2xl': '1100px',
+      '3xl': '1440px',
+      '4xl': '1550px',
+      widescreen: {
+        raw: '(min-aspect-ratio: 3/2)',
+      },
+      tallscreen: { raw: '(max-aspect-ratio: 13/20)' },
+    },
     extend: {
       colors: {
         'primary-1': 'hsl(257, 40%, 49%)',
-        'primary-2': 'hsl(300, 69%, 71%)'
+        'primary-2': 'hsl(300, 69%, 71%)',
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
